74b078c351ec7367249091af96695745029973c6
cjgochanour <cjgochanour@gmail.com>
2021-11-16 13:35:33 -0600

    added database.js, explorer ch1 complete

---
+++ scripts/database.js
@@ -0,0 +1,33 @@
+const database = {
+    fish: [
+        {
+            name: "Mittens",
+            species: "Catfish",
+            location: "Rainforests",
+            length: 600,
+            diet: "Tuna",
+        },
+        {
+            name: "Bigsby",
+            species: "Colossal-Mouth Bass",
+            location: "Earth",
+            length: 40000,
+            diet: "Crab, Manwich",
+        },
+        {
+            name: "Littlesly",
+            species: "Jumbo Shrimp",
+            location: "Red Lobster",
+            length: 6.4,
+            diet: "Java Moss",
+        },
+        {
+            name: "Paul",
+            species: "Merman",
+            location: "Iceland",
+            length: 183,
+            diet: "Protein Bars",
+        },
+
+    ]
+}
